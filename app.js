ce0f67acdc370d8348ad6acc21417183f786fb1c
Amitabh Tripathi <tripathi75@yahoo.com>
2020-09-16 17:42:49 +0530

    message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send("Welcome to my portal, my friend");
+	res.send("Welcome to my portal, Aadi");
 });
 
 const port = process.env.port || 3000;
