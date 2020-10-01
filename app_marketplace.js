801a6acb2cbd784917ca25dcf90c5ea2e49055d2
Amitabh Tripathi <tripathi75@yahoo.com>
2020-10-01 21:09:29 +0530

    removed mongodb connection

--- app_marketplace.js
+++ app_marketplace.js
@@ -8,11 +8,11 @@ var flash = require('connect-flash');
 var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+//var mongo = require('mongodb');
+//var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/assignment');
-var db = mongoose.connection;
+//mongoose.connect('mongodb://localhost/assignment');
+//var db = mongoose.connection;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
